bb7d09d4b50e3c11b7e18f8c2822a1a5d588eeb7
Feng Mai <maifeng@gmail.com>
2016-06-25 20:39:52 -0400

    Updated to reflex changes in the amazon website. Amazon is no longer displaying total number of helpful votes.

--- src/Item.java
+++ src/Item.java
@@ -18,6 +18,7 @@ import java.security.NoSuchAlgorithmException;
 import java.sql.*;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -48,6 +49,35 @@ public class Item {
 		reviews.add(thereview);
 	}
 
+    /**
+     * retry the Jsoup connection
+     * @param url
+     * @return
+     */
+    public org.jsoup.nodes.Document jsoup_load_with_retry(String url) throws IOException {
+        int max_retry = 10;
+        int retry = 1;
+        int sleep_sec = 2;
+        org.jsoup.nodes.Document content = null;
+
+        while(retry <= max_retry){
+            try {
+                content = Jsoup.connect(url).timeout(10 * 1000).get();
+                break;
+            } catch (Exception ex){
+                //wait before retry
+                System.out.println(ex.getMessage() + " retrying..");
+                try {
+                    TimeUnit.SECONDS.sleep(sleep_sec);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            retry++;
+        }
+        return content;
+    }
+
 	/**
 	 * Fetch all reviews for the item from Amazon.com
 	 */
@@ -57,7 +87,8 @@ public class Item {
 		try {
 			// Get the max number of review pages;
 			org.jsoup.nodes.Document reviewpage1 = null;
-			reviewpage1 = Jsoup.connect(url).timeout(10*1000).get();
+//			reviewpage1 = Jsoup.connect(url).timeout(10*1000).get();
+            reviewpage1 = jsoup_load_with_retry(url);
 			int maxpage = 1;
 			Elements pagelinks = reviewpage1.select("a[href*=pageNumber=]");
 			if (pagelinks.size() != 0) {
@@ -77,8 +108,9 @@ public class Item {
 						+ "/?sortBy=helpful&pageNumber="
 						+ p;
 				org.jsoup.nodes.Document reviewpage = null;
-                reviewpage = Jsoup.connect(url).timeout(10*1000).get();
-				if (reviewpage.select("div.a-section.review").isEmpty()) {
+//                reviewpage = Jsoup.connect(url).timeout(10*1000).get();
+                reviewpage = jsoup_load_with_retry(url);
+                if (reviewpage.select("div.a-section.review").isEmpty()) {
 					System.out.println(itemID + " " + "no reivew");
 				} else {
 					Elements reviewsHTMLs = reviewpage.select(
@@ -91,8 +123,9 @@ public class Item {
 
 			}
 
-		} catch (Exception e) {
-			System.out.println(itemID + " " + "Exception" + " " + e.getClass());
+		}
+		catch (Exception e) {
+			System.out.println(itemID + " " + "Exception" + " " + e.toString());
 		}
 
 	}
@@ -114,68 +147,76 @@ public class Item {
 		int rating = 0;
 		int fullRating = 5;
 		int helpfulVotes = 0;
-		int totalVotes = 0;
+		int totalVotes = -1; // Note: 2016-06-25: Amazon no longer shows the total helpful votes
 		boolean verifiedPurchase = false;
-		String realName = "N/A"; //Note 2015-11-14 : Amazon seems to got rid of the real name badge
-
+		String realName = "N/A"; //Note 2015-11-14 : Amazon no longer shows the real name badge
+		Date reviewDate = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
+				.parse("January 1, 1900");
 		String content = "";
 
+
 		// review id
-		reviewID = reviewBlock.id();
-//        System.out.println(reviewID);
-        // customer name and id
-		Elements customerIDs = reviewBlock.getElementsByAttributeValueContaining(
-				"href", "/gp/pdp/profile/");
-		if (customerIDs.size() > 0) {
-			Element customer = customerIDs.first();
-			String customerhref = customer.attr("href");
-			String patternString = "(/gp/pdp/profile/)(.+)";
-			Pattern pattern = Pattern.compile(patternString);
-			Matcher matcher = pattern.matcher(customerhref);
-			matcher.find();
-			// cutomer id;
-			customerID = matcher.group(2);
-			// customer name;
-			customerName = customer.text();
-        }
-		// title
-		Element reviewTitle = reviewBlock.select("a.review-title").first();
-		title = reviewTitle.text();
+			reviewID = reviewBlock.id();
+		try {
+			// customer name and id
+			Elements customerIDs = reviewBlock.getElementsByAttributeValueContaining(
+					"href", "/gp/pdp/profile/");
+			if (customerIDs.size() > 0) {
+				Element customer = customerIDs.first();
+				String customerhref = customer.attr("href");
+				String patternString = "(/gp/pdp/profile/)(.+)(/)";
+				Pattern pattern = Pattern.compile(patternString);
+				Matcher matcher = pattern.matcher(customerhref);
+				while(matcher.find()) {
+					// cutomer id;
+					customerID = matcher.group(2);
+					// customer name;
+					customerName = customer.text();
+				}
+			}
+			// title
+			Element reviewTitle = reviewBlock.select("a.review-title").first();
+			title = reviewTitle.text();
 
-		// rating
-		Element star = reviewBlock.select("i.a-icon-star").first();
-		String starinfo = star.text();
-		rating = Integer.parseInt(starinfo.substring(0, 1));
+			// rating
+			Element star = reviewBlock.select("i.a-icon-star").first();
+			String starinfo = star.text();
+			rating = Integer.parseInt(starinfo.substring(0, 1));
 
-		// usefulness voting
-		Elements votes = reviewBlock.select("span.review-votes");
-		if (votes.size() > 0) {
-			String votingtext = votes.first().text();
-			Pattern pattern2 = Pattern.compile("(\\S+)( of )(\\S+)");
-			Matcher matcher2 = pattern2.matcher(votingtext);
-			matcher2.find();
-			// customer id;
-			helpfulVotes = Integer.parseInt(matcher2.group(1).replaceAll(",", ""));
-			totalVotes = Integer.parseInt(matcher2.group(3).replaceAll(",", ""));
+			// usefulness voting
+			Elements votes = reviewBlock.select("span.review-votes");
+			if (votes.size() > 0) {
+				String votingtext = votes.first().text();
+				Pattern pattern2 = Pattern.compile("(\\S+)( people found this helpful)");
+				Matcher matcher2 = pattern2.matcher(votingtext);
+				matcher2.find();
+				while(matcher2.find()) {
+					helpfulVotes = Integer.parseInt(matcher2.group(1).replaceAll(",", ""));
+				}
+//			totalVotes = Integer.parseInt(matcher2.group(3).replaceAll(",", ""));
+			}
+
+			// verified purchase
+			Elements verified = reviewBlock.select("span.a-size-mini:contains(Verified Purchase)");
+			if (verified.size() > 0) {
+				verifiedPurchase = true;
+			}
+
+
+			// review date
+			Elements date = reviewBlock.select("span.review-date");
+			String datetext = date.first().text();
+			datetext = datetext.substring(3); // remove "On "
+			reviewDate = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
+					.parse(datetext);
+
+			// review content
+			Element contentDoc = reviewBlock.select("span.review-text").first();
+			content = contentDoc.text();
 		}
-
-		// verified purchase
-		Elements verified = reviewBlock.select("span.a-size-mini:contains(Verified Purchase)");
-		if (verified.size() > 0){
-            verifiedPurchase = true;
-        }
-
-
-		// review date
-		Elements date = reviewBlock.select("span.review-date");
-        String datetext = date.first().text();
-        datetext = datetext.substring(3); // remove "On "
-        Date reviewDate = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH)
-				.parse(datetext);
-
-		// review content
-		Element contentDoc = reviewBlock.select("span.review-text").first();
-		content = contentDoc.text();
+		catch (Exception e) {
+			System.out.println(reviewID + " " + "Exception" + " " + e.toString());
+		}
 		Review thereview = new Review(theitemID, reviewID, customerName,
 				customerID, title, rating, fullRating, helpfulVotes,
 				totalVotes, verifiedPurchase, realName, reviewDate, content);
